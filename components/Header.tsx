f2900ae3ab542a82d433a665e3136256daeb98a2
Caleblan <calandry1@hotmail.ca>
2023-02-14 17:48:28 -0800

    Just some updates

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,16 +16,17 @@ import { Menu } from '@mui/icons-material';
 export default function Header() {
     
     return (
-        <nav className="w-full flex items-center mx-4">
+        <nav className="w-full flex items-center mx-4 my-4">
             <div className="w-1/2 flex text-3xl">
-                <Link href="/">Header</Link>
+                <Link href="/">Hatsuon</Link>
             </div>
-            <div className="w-1/2 flex justify-end space-around m:invisible">
+            <div className="w-1/2 flex justify-end space-around">
 
-                <Link href="/dictionary">Dictionary</Link>
-                <Link href="/pitchGenerator">Pitch Accent Diagram Generator</Link>
-
-                <Button>
+                <div className="md:visible invisible flex gap-x-4">
+                    <Link href="/dictionary">Dictionary</Link>
+                    <Link href="/pitchGenerator">Pitch Accent Diagram Generator</Link>
+                </div>
+                <Button className="md:invisible visible">
                     <SvgIcon>
                         <Menu/>
                     </SvgIcon>
