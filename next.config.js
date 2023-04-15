74feadd7fcc31062f83795723dee76f35aa67142
Caleblan <calandry1@hotmail.ca>
2023-04-15 02:14:35 -0700

    adad

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
-  assetPrefix: './',
 }
 
 module.exports = nextConfig
